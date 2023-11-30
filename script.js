e3ac623d71b0a177316ac76249f35c7ff55760f3
Tiego Ouedraogo <tiegoouedraogo@gmail.com>
2023-11-30 18:12:24 -0500

    part 1 2 3 4 done

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-/**
- * Part 1: Refactoring Old Code
-When code is outdated or inefficient, it often goes through a process called “refactoring.” 
-Refactoring code is the process of restructuring that code without changing its original behavior.
-For the first part of this assignment, revisit your code from ALAB 308.3.1, 
-wherein you create a script that parsed CSVs. Now that you have knowledge of arrays and objects, 
-how would you change your approach to this problem? Take a few minutes to examine and refactor the code before continuing.
-For reference, ALAB 308.3.1 is embedded below. The section on CSV parsing is “Part 3.”
- */
+//Part1
 // Split the CSV string into rows based on newline characters.
 const csvString = `ID,Name,Occupation,Age
 42,Bruce,Knight,41
@@ -22,26 +14,7 @@ for (let i = 0; i < rows.length; i++) {
     console.log(cell1, cell2, cell3, cell4);
 }
 
-/** Part2
- * 
-Store your results in a two-dimensional array.
-Each row should be its own array, with individual entries for each column.
-Each row should be stored in a parent array, with the heading row located at index 0.
-Cache this two-dimensional array in a variable for later use.
-Using the original CSV example data, here is what the result of this step should look like:
-
-ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,5
-8\n98,Bill,Doctor’s Assistant,26
-becomes
-[
-["ID", "Name", "Occupation", "Age"],
-["42", "Bruce", "Knight", "41"],
-["57", "Bob", "Fry Cook", "19"],
-["63", "Blaine", "Quiz Master", "58"],
-["98", "Bill", "Doctor’s Assistant", "26"]
-]
- * */ 
-
+//Part2
 let lines = csvString.split('\n');
 let colNum = lines[0].split(',').length;
 let twoD = [];
@@ -52,33 +25,7 @@ for(let line of lines){
 }
 console.log(twoD);
 
-/** Part3
- * For each row of data in the result array produced by your code above, 
- * create an object where the key of each value is the heading for that value’s column.
-Convert these keys to all lowercase letters for consistency.
-Store these objects in an array, in the order that they were originally listed.
-Since the heading for each column will be stored in the object keys, 
-you do not need to create an object for the heading row itself.
-For instance, the results of the example data above being passed through this step are as follows:
-[["ID", "Name", "Occupation", "Age"],
-["42", "Bruce", "Knight", "41"],
-["57", "Bob", "Fry Cook", "19"],
-["63", "Blaine", "Quiz Master", "58"],
-["98", "Bill", "Doctor’s Assistant", "26"]
-]
-becomes
-[
-{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
-{ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
-{ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
-{ id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26" }
-]
-{id: "42", name: "Bruce", occupation: "Knight", age: "41"}
-1 {id: "57", name: "Bob", occupation: "Fry Cook", age: "19"}
-2 {id: "63", name: "Blaine", occupation: "Quiz Master", age: "58"}
-3 {id: "98", name: "Bill", occupation: "Doctor’s Assistant", age: "26"}
-*/
-//let lines = csvString.split('\n');
+//Part 3
 //using const map1 = array1.map((x) => x * 2);
 //let header = lines[0].split(',');
 //let result = [];
@@ -92,31 +39,8 @@ for(let j=0; j< header.length;j++){
 }
 console.log(result);
 
-/**Part4
- * Part 4: Sorting and Manipulating Data
-It is important to know how to work with data in this format, an array of objects, 
-as it is one of the most commonly used data formats in JavaScript.
-Using array methods, accomplish the following tasks, in order upon the result of Part 3:
-1-Remove the last element from the sorted array.
-2-Insert the following object at index 1:
-{ id: "48", name: "Barry", occupation: "Runner", age: "25" }
 
-Add the following object to the end of the array:
-{ id: "7", name: "Bilbo", occupation: "None", age: "111" }
-
-So far, the results should look like this:
-
-[{ id: "42", name: "Bruce", occupation: "Knight", age: "41" },
-{ id: "48", name: "Barry", occupation: "Runner", age: "25" },
-{ id: "57", name: "Bob", occupation: "Fry Cook", age: "19" },
-{ id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" },
-{ id: "7", name: "Bilbo", occupation: "None", age: "111" }]
-
-Finally, use the values of each object within the array and the array’s 
-length property to calculate the average age of the group. 
-This calculation should be accomplished using a loop.
-*/
-
+//Part 4
 //let lines = csvString.split('\n');
 //using const map1 = array1.map((x) => x * 2);
 let header = lines[0].split(',').map(header => header.toLowerCase());;
